140b32b2f0e69cfc9fed378014a8bce61f925d00
Dercio Maduna <12derciomaduna@gmail.com>
2023-03-14 20:24:55 +0000

    Update product-item.jsx

--- components/product/product-item.jsx
+++ components/product/product-item.jsx
@@ -38,33 +38,33 @@ export default function ProductItem(props) {
   // similarProducts ]  = React.useState()
 
   console.log("length :", props.product && props.products.length);
-  props.products &&
-    props.products.map((item) => {
-      localProduct.Categories.map((localItem) => {
-        if (item.Categories.includes(localItem)) {
-          if (similarProducts.length === 0) {
-            similarProducts.push(item);
-            console.log(similarProducts);
-            return;
-          } else if (similarProducts.length > 0) {
-            similarProducts.map((similarProduct) => {
-              if (JSON.stringify(similarProduct) === JSON.stringify(item)) {
-                console.log("skip this one", similarProduct.Title);
-              }
-            });
-          } else {
-            similarProducts.map((similarProduct) => {
-              console.log("Here", similarProduct.Title);
-              if (similarProduct.Title !== item.Title) {
-                similarProducts.push(item);
-                console.log(similarProducts);
-                return;
-              }
-            });
-          }
-        }
-      });
-    });
+  // props.products &&
+  //   props.products.map((item) => {
+  //     localProduct.Categories.map((localItem) => {
+  //       if (item.Categories.includes(localItem)) {
+  //         if (similarProducts.length === 0) {
+  //           similarProducts.push(item);
+  //           console.log(similarProducts);
+  //           return;
+  //         } else if (similarProducts.length > 0) {
+  //           similarProducts.map((similarProduct) => {
+  //             if (JSON.stringify(similarProduct) === JSON.stringify(item)) {
+  //               console.log("skip this one", similarProduct.Title);
+  //             }
+  //           });
+  //         } else {
+  //           similarProducts.map((similarProduct) => {
+  //             console.log("Here", similarProduct.Title);
+  //             if (similarProduct.Title !== item.Title) {
+  //               similarProducts.push(item);
+  //               console.log(similarProducts);
+  //               return;
+  //             }
+  //           });
+  //         }
+  //       }
+  //     });
+  //   });
 
   return (
     <Paper
@@ -279,7 +279,7 @@ export default function ProductItem(props) {
                 <Box
                   sx={{ display: "flex", justifyContent: "", width: "100%" }}
                 >
-                  {props.product &&
+                  {/* {props.product &&
                     props.product.Sizes.map((item) => {
                       return (
                         <Button
@@ -301,7 +301,7 @@ export default function ProductItem(props) {
                           {item}
                         </Button>
                       );
-                    })}
+                    })} */}
                 </Box>
                 <Typography
                   sx={{
@@ -324,7 +324,7 @@ export default function ProductItem(props) {
                     padding: "12px 16px",
                   }}
                 >
-                  {props.product &&
+                  {/* {props.product &&
                     props.product.Colors.map((item) => {
                       return (
                         <Button
@@ -357,7 +357,7 @@ export default function ProductItem(props) {
                           />
                         </Button>
                       );
-                    })}
+                    })} */}
                 </Box>
 
                 <TextField
